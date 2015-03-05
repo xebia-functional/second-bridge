9f6f1d7d6231e158cbd318829f2dba7ee61dbdc8
Ana <ana@47deg.com>
2015-03-05 12:36:14 +0100

    Framework structure

---
+++ Swift47Deg/Swift47Deg/Swift47Deg.h
@@ -0,0 +1,19 @@
+//
+//  Swift47Deg.h
+//  Swift47Deg
+//
+//  Created by Ana on 5/3/15.
+//  Copyright (c) 2015 47 Degrees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift47Deg.
+FOUNDATION_EXPORT double Swift47DegVersionNumber;
+
+//! Project version string for Swift47Deg.
+FOUNDATION_EXPORT const unsigned char Swift47DegVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift47Deg/PublicHeader.h>
+
+
---
+++ Swift47Deg/Swift47DegTests/Swift47DegTests.swift
@@ -0,0 +1,38 @@
+//
+//  Swift47DegTests.swift
+//  Swift47DegTests
+//
+//  Created by Ana on 5/3/15.
+//  Copyright (c) 2015 47 Degrees. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import Swiftz
+
+
+class Swift47DegTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
