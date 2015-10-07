8d5c67e54e7705619e312a2a2920bdb096ac21b7
Javier de Silóniz Sandino <javier.s.s@47deg.com>
2015-10-07 16:04:09 +0200

    Implemented flatmap, map and filter

--- SecondBridge/SecondBridge/ErrorHandling/Try.swift
+++ SecondBridge/SecondBridge/ErrorHandling/Try.swift
@@ -13,18 +13,24 @@ enum TryMatcher<T> {
     case Failure(ErrorType)
 }
 
+enum TryError : ErrorType {
+    case TryFilterException
+}
+
 struct Try<T> {
-    private let operation : (() throws -> T)
     private let matchResult : TryMatcher<T>
     
-    init(op: (() throws -> T)) {
-        operation = op
+    init(@autoclosure op: (() throws -> T)) {
         do {
             let result = try op()
             matchResult = TryMatcher<T>.Success(result)
         } catch let e {
             matchResult = TryMatcher<T>.Failure(e)
         }
+    }
+    
+    init(m: TryMatcher<T>) {
+        matchResult = m
     }
     
     func match() -> TryMatcher<T> {
@@ -50,10 +56,10 @@ struct Try<T> {
         return nil
     }
     
-    func getOrElse() -> T? {
+    func getOrElse(def: T) -> T {
         switch self.match() {
         case .Success(let value): return value
-        case .Failure(_): return nil
+        case .Failure(_): return def
         }
     }
     
@@ -61,5 +67,28 @@ struct Try<T> {
         throw(ex)
     }
     
+    func map<B>(f: T -> B) -> Try<B> {
+        switch self.match() {
+        case .Success(let value): return Try<B>(m: TryMatcher.Success(f(value)))
+        case .Failure(let ex): return Try<B>(m: TryMatcher.Failure(ex))
+        }
+    }
     
+    func flatMap<B>(f: T -> Try<B>) -> Try<B> {
+        switch self.match() {
+        case .Success(let value): return f(value)
+        case .Failure(let ex): return Try<B>(m: TryMatcher.Failure(ex))
+        }
+    }
+    
+    func filter(f: T -> Bool) -> Try {
+        switch self.match() {
+        case .Success(let value):
+            if f(value) {
+                return self
+            }
+            return Try(m: TryMatcher.Failure(TryError.TryFilterException))
+        case .Failure(_): return self
+        }
+    }
 }
--- SecondBridge/SecondBridgeTests/ErrorHandling/TryTests.swift
+++ SecondBridge/SecondBridgeTests/ErrorHandling/TryTests.swift
@@ -14,6 +14,10 @@ class TryTests: XCTestCase {
         case InvalidString
     }
     
+    enum OperationError: ErrorType {
+        case DivideByZero
+    }
+    
     func convertStringToInt(s: String) throws -> Int {
         if let parsedInt = Int(s) {
             return parsedInt
@@ -21,20 +25,69 @@ class TryTests: XCTestCase {
             throw ParseError.InvalidString
         }
     }
+    
+    func halfOf(n: Int) -> Try<Int> {
+        return Try<Int>(op: (n / 2))
+    }
+    
+    func divideInt(n: Int, by: Int) throws -> Int {
+        switch by {
+        case 0: throw OperationError.DivideByZero
+        default: return n / by
+        }
+    }
+    
+    func tryDivideByZeroWhoops(n: Int) -> Try<Int> {
+        return Try<Int>(op: (try divideInt(n, by: 0)))
+    }
+    
+    func tryHalf(n: Int) -> Try<Int> {
+        return Try<Int>(op: (try divideInt(n, by: 2)))
+    }
 
     func testTryBasicBehaviour() {
-        let tryParseCorrectString = Try<Int>(op: {() throws -> Int in try self.convertStringToInt("47")})
+        let tryParseCorrectString = Try<Int>(op: try self.convertStringToInt("47"))
         XCTAssertTrue(tryParseCorrectString.isSuccess(), "Correct operation inside a Try should be a success")
         XCTAssertFalse(tryParseCorrectString.isFailure(), "Correct operation inside a Try shouldn't be a failure")
-        let value = tryParseCorrectString.getOrElse()
+        let value = tryParseCorrectString.get()
         XCTAssertNotNil(value, "Correct operation inside a Try should yield a value")
         XCTAssertEqual(value, 47, "Correct operation inside a Try should yield the expected value")
         
-        let tryParseIncorrectString = Try<Int>(op: {() throws -> Int in try self.convertStringToInt("47 Degrees")})
+        let tryParseIncorrectString = Try<Int>(op: try self.convertStringToInt("47 Degrees"))
         XCTAssertFalse(tryParseIncorrectString.isSuccess(), "Invalid operation inside a Try shouldn't be a success")
         XCTAssertTrue(tryParseIncorrectString.isFailure(), "Invalid operation inside a Try should be a failure")
-        let invalidValue = tryParseIncorrectString.getOrElse()
-        XCTAssertNil(invalidValue, "Invalid operation inside a Try shouldn't yield a value")
+        let invalidValue = tryParseIncorrectString.getOrElse(666)
+        XCTAssertEqual(invalidValue, 666, "Invalid operation inside a Try shouldn't yield a value")
+    }
+    
+    func testTryHigherOrderFunctions() {
+        let tryParseCorrectString = Try<Int>(op: try self.convertStringToInt("47"))
+        let tryParseIncorrectString = Try<Int>(op: try self.convertStringToInt("47 Degrees"))
+        
+        let mapCorrectResult = tryParseCorrectString.map({ $0 + 10 }).getOrElse(666)
+        XCTAssertEqual(mapCorrectResult, 57, "Mapping over a successful Try should yield a correct value")
+        
+        let mapIncorrectResult = tryParseIncorrectString.map({ $0 + 10 }).getOrElse(666)
+        XCTAssertEqual(mapIncorrectResult, 666, "Mapping over a failed Try shouldn't yield a correct value")
+        
+        let filterCorrectResult = tryParseCorrectString.filter({ $0 != 47 })
+        XCTAssert(filterCorrectResult.isFailure(), "A Success filtered with an invalid predicate should be changed to a Failure")
+        
+        let filterIncorrectResult = tryParseIncorrectString.filter({ $0 != 47 })
+        XCTAssert(filterIncorrectResult.isFailure(), "A Failure filtered with any predicate should be returned as such")
+        
+        let flatmapCorrectResultAgainstWrongFunction = tryParseCorrectString.flatMap(tryDivideByZeroWhoops)
+        XCTAssertTrue(flatmapCorrectResultAgainstWrongFunction.isFailure(), "When flatmapping a Success against a Failure, we should get a Failure")
+        
+        let flatmapCorrectResultAgainstOKFunction = tryParseCorrectString.flatMap(tryHalf)
+        XCTAssertTrue(flatmapCorrectResultAgainstOKFunction.isSuccess(), "When flatmapping a Success against a Success, we should get a Success")
+        XCTAssertEqual(flatmapCorrectResultAgainstOKFunction.getOrElse(1), 23, "When flatmapping a Success against a Success, we should get a Success of a valid value")
+        
+        let flatmapIncorrectResultAgainstOKFunction = tryParseIncorrectString.flatMap(tryHalf)
+        XCTAssertTrue(flatmapIncorrectResultAgainstOKFunction.isFailure(), "When flatmapping a Failure against a Success, we should get a Failure")
+        
+        let flatmapIncorrectResultAgainstIncorrectFunction = tryParseIncorrectString.flatMap(tryDivideByZeroWhoops)
+        XCTAssertTrue(flatmapIncorrectResultAgainstIncorrectFunction.isFailure(), "When flatmapping a Failure against a Failure, we should get a Failure")        
     }
 
 }
